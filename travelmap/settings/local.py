7d0ebb6eb40674027fcd3fad82eb6d5484b7cc3f
Igor Gitmn <igor.a.gitman@gmail.com>
2019-09-24 17:14:53 +0700

    Split settings on local and production

---
+++ travelmap/settings/local.py
@@ -0,0 +1,14 @@
+"""Local settings used for developing"""
+
+from travelmap.settings.base import *
+
+
+DEBUG = True
+ALLOWED_HOSTS = []
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
---
+++ travelmap/settings/production.py
@@ -0,0 +1,36 @@
+"""Production django settings"""
+
+from travelmap.settings.base import *
+
+
+DEBUG = False
+ALLOWED_HOSTS = ['travelmap.igitman.com', 'www.travelmap.igitman.com']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {'read_default_file': os.path.join(BASE_DIR, 'db.conf'),
+                    'charset': 'utf8mb4'},
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'WARNING',
+            'class': 'logging.FileHandler',
+            'filename': '../logs/django.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'WARNING',
+            'propagate': True,
+        },
+    },
+}
