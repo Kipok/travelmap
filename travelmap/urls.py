f7745be49c127a0b481e088d896c6c7b536a8037
Igor Gitmn <igor.a.gitman@gmail.com>
2019-09-24 17:31:47 +0700

    Add basic map with basic markers

--- travelmap/urls.py
+++ travelmap/urls.py
@@ -5,8 +5,11 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from map.views import map_view
+
 
 urlpatterns = [
+    path('', map_view, name='map'),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
   + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
