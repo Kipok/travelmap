6771838d8fe5c79cdd1872ee1b5c13cca95d236d
Igor Gitmn <igor.a.gitman@gmail.com>
2019-09-27 20:49:52 +0700

    Add form for new travel

---
+++ map/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from .models import TravelRecord
+
+
+class TravelRecordForm(forms.ModelForm):
+    class Meta:
+        model = TravelRecord
+        fields = ['place_name', 'desc', 'start_date', 'end_date', 'lon', 'lat']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['place_name'].widget.attrs['class'] = 'form-control'
+        self.fields['place_name'].widget.attrs['placeholder'] = 'Название места'
+        self.fields['desc'].widget.attrs['class'] = 'form-control'
+        self.fields['desc'].widget.attrs['placeholder'] = 'Что мы помним?'
+        self.fields['start_date'].widget.attrs['class'] = 'form-control'
+        self.fields['start_date'].widget.attrs['placeholder'] = 'Дата начала'
+        self.fields['end_date'].widget.attrs['class'] = 'form-control'
+        self.fields['end_date'].widget.attrs['placeholder'] = 'Дата окончания'
+        self.fields['lon'].widget.attrs['class'] = 'form-control small-form'
+        self.fields['lon'].widget.attrs['placeholder'] = 'Долгота'
+        self.fields['lat'].widget.attrs['class'] = 'form-control small-form'
+        self.fields['lat'].widget.attrs['placeholder'] = 'Широта'
+
--- map/views.py
+++ map/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 
 from .models import TravelRecord
+from .forms import TravelRecordForm
 
 
 def map_view(request):
-    records = TravelRecord.objects.all()
+    success_text = ""
+    form = TravelRecordForm(request.POST or None, request.FILES or None)
+    if request.method == 'POST' and form.is_valid():
+        record = form.save(commit=False)
+        # ...
+        # record.save()
+        success_text = "Путешествие добавлено! :)"
+
     context = {
-        "records": records,
+        "form": form,
+        "records": TravelRecord.objects.all(),
+        "success_text": success_text,
     }
     return render(request, "map/map.html", context)
