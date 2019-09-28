9634af486921e95a1ced32a7dcc1ffd2201e35eb
Igor Gitmn <igor.a.gitman@gmail.com>
2019-09-28 20:37:56 +0700

    Add image resize

--- map/views.py
+++ map/views.py
@@ -1,17 +1,37 @@
+from io import BytesIO
+from PIL import Image
+
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.core.files.base import ContentFile
+from django.core.files.uploadedfile import InMemoryUploadedFile
 
 from .models import TravelRecord, TravelImage
 from .forms import TravelRecordForm
 
 
+def resize_image(image_data):
+    """Helper function to resize an image"""
+    img = Image.open(image_data)
+    img.thumbnail([1200, 1200], Image.ANTIALIAS)
+    buffer = BytesIO()
+    img_format = image_data.content_type.split('/')[1]
+    if img_format == 'apng':
+        img_format = 'png'
+    img.save(fp=buffer, format=img_format)
+    img_file = ContentFile(buffer.getvalue())
+    return InMemoryUploadedFile(img_file, None, image_data.name,
+                                image_data.content_type, img_file.tell, None)
+
+
 def map_view(request):
-    success_text = ""
     form = TravelRecordForm(request.POST or None, request.FILES or None)
     if request.method == 'POST' and form.is_valid():
         travel_record = form.save()
         for photo in request.FILES.getlist('photos'):
+            photo = resize_image(photo)
             TravelImage.objects.create(photo=photo, travel=travel_record)
-        success_text = "Путешествие добавлено! :)"
+        return HttpResponseRedirect('')
 
     records = TravelRecord.objects.all().order_by('start_date')
     records_dict = {}
@@ -23,6 +43,5 @@ def map_view(request):
         "form": form,
         "records_dict": records_dict,
         "records": records,
-        "success_text": success_text,
     }
     return render(request, "map/map.html", context)
